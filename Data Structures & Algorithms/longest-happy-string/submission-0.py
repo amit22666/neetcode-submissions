1436889f5e2b02db3852c84731abe6a981cfff50
Amit kumar <38582958+amit22666@users.noreply.github.com>
2026-08-15 18:24:24 +0530

    Add: longest-happy-string - submission-0

---
+++ Data Structures & Algorithms/longest-happy-string/submission-0.py
@@ -0,0 +1,33 @@
+class Solution:
+    def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        res = ""
+        maxHeap = []
+        # negative because maxHeap
+        # a is a ka count that is given as input param
+        for count, char in [(-a, "a"), (-b, "b"), (-c, "c")]:
+            if count != 0:
+                heapq.heappush(maxHeap, (count, char))
+
+        while maxHeap:
+            count, char = heapq.heappop(maxHeap)
+            # previous 2 same character ka check
+            # len(res) > 1 , atleat 2 characters hai?
+            # last 2 character same hai?
+            if len(res) > 1 and res[-1] == res[-2] == char:
+                if not maxHeap: 
+                    break # break 
+                # pop second most common/frequent character
+                count2, char2 = heapq.heappop(maxHeap)
+                res += char2
+                count2 += 1 # decrement the count2 - 1 (since it is a max heap and count are negative so we have added 1)
+
+                # if count exist then add 2nd MFC character to max heap
+                if count2:
+                    heapq.heappush(maxHeap, (count2, char2))
+            else:
+                res += char
+                count += 1 # decrement the count - 1 (since it is a max heap and count are negative so we have added 1)
+            if count:
+                heapq.heappush(maxHeap, (count, char))
+
+        return res
